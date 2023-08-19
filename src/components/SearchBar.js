2f4fdc29333b6ff2d12409809019b5ab5d15e95d
Nathália Brum <natybrum.97@gmail.com>
2023-08-19 08:57:20 -0300

    feat/versão com busca

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,51 @@
+import React, { useState } from "react";
 import { FiSearch } from "react-icons/fi";
 import { styled } from "styled-components";
+import { DebounceInput } from "react-debounce-input";
+import axios from "axios";
 
 export default function SearchBar() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = async (value) => {
+    setSearchTerm(value);
+
+    console.log(value);
+
+    if (value && value.length >= 3) {
+     
+      axios.get(`${process.env.REACT_APP_API_URL}/search-users?query=${value}`).then((response) => {
+      console.log(response.data);
+      setSearchResults(response.data);
+    }).catch((error) => {
+      console.error(error);
+      setSearchResults([]); 
+    });
+  } else {
+    setSearchResults([]);
+  }
+
+}
+
   return (
     <Container>
-      <input data-test="search" type="search" placeholder="Search for people" />
+      <DebounceInput
+        data-test="search"
+        type="search"
+        placeholder="Search for people"
+        debounceTimeout={300} // Aguarde 300ms após a última digitação para iniciar a busca
+        onChange={(e) => handleSearch(e.target.value)}
+      />
       <FiSearch color="#C6C6C6" size={34} cursor="pointer" />
+      <ul>
+        {searchResults.map((result) => (
+          <li key={result.id}>
+          <img src={result.pictureUrl} alt={`${result.username}'s profile`} />
+          <span>{result.username}</span>
+        </li>
+        ))}
+      </ul>
     </Container>
   );
 }
@@ -13,8 +53,7 @@ export default function SearchBar() {
 const Container = styled.form`
   height: 25px;
   display: flex;
-  justify-content: space-between;
-  align-items: center;
+  flex-direction: column;
   gap: 10px;
   margin: 0 15px;
   padding: 10px;
@@ -29,6 +68,29 @@ const Container = styled.form`
 
     &:focus {
       outline: 0;
+    }
+  }
+
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+
+    li {
+      display: flex; /* Exibir foto e nome lado a lado */
+      align-items: center;
+      gap: 10px;
+
+      img {
+        width: 40px; /* Ajuste conforme necessário */
+        height: 40px; /* Ajuste conforme necessário */
+        border-radius: 50%;
+      }
+
+      span {
+        font-size: 16px;
+        color: white;
+      }
     }
   }
 `;
